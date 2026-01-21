bfcab799c1676d43070f8808542e641dd711e482
Rajat Negi <007negirajat@gmail.com>
2026-01-21 23:22:05 +0530

    Added syntax and rules folder

--- syntax&rules.py/indentation.py
+++ syntax&rules.py/indentation.py
@@ -1,2 +1,11 @@
+
 #Indentation means the spaces at the beginning of a line of code.
-In Python, indentation is not optional — it is used to define code blocks.
+#In Python, indentation is not optional — it is used to define code blocks.
+#If a line ends with a colon : → the next line must be indented.
+
+#eg-
+marks=int(input("enter marks: "))
+if marks > 40:      # Colon → indent starts
+    print("Pass")   # Indented
+
+print("Done")       # No colon above → no indent
